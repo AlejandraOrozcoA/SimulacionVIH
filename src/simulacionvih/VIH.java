3adce4645d684f7bc20e4f8207a6026293f5809b
Alejandra <aorozcoa1400@alumno.ipn.mx>
2021-05-14 23:54:59 -0500

    Se implemento el algoritmo, ya pinta puntos

--- src/simulacionvih/VIH.java
+++ src/simulacionvih/VIH.java
@@ -1,17 +1,29 @@
 
 package simulacionvih;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class VIH {
+    private final int rb = 4;
+    private final int lag = 4;
+    private final int prepl = 99;
+    private final int pinfect = 10000;
+    private final int pvih = 5;
     private int alto;
     private int ancho;
     private char actual[][];
     private char siguiente[][];
+    private int lagActual [][];
+
     
     public VIH(int alto, int ancho) {
         this.alto = alto;
         this.ancho = ancho;
         actual = new char [alto][ancho];
         siguiente= new char [alto][ancho];
+        lagActual = new int [alto][ancho];
         llenar();
     }
 
@@ -31,14 +43,283 @@ public class VIH {
         this.siguiente[y][x]= l;
     }
 
-    private void llenar() {
+    public int getLagActual(int x, int y) {
+        return lagActual[y][x];
+    }
+
+    public void setLagActual(int x, int y, boolean bandera) {
+        if (bandera) {
+            lagActual[y][x] = lagActual[y][x]+1;
+        }else {
+            lagActual[y][x]= 0;
+        }
+    }
+    
+    //Inicializa los arreglos actual y siguiente
+    public void llenar() {
         for (int i = 0; i < alto ; i++) {
             for (int j = 0; j < ancho ; j++) {
-                actual[i][j]= 'H';
-                siguiente[i][j]= 'H';
+                this.setActual(j, i, 'H');
+                this.setSiguiente(j, i,'H');
+                this.setLagActual(j, i, false);
+            }
+        }
+    }
+        
+    public char definirEstadoSig(int y, int x) {
+        int aux = 0;
+        char aux2 = this.getActual(x, y);
+        switch (aux2) {
+            case 'H':
+                if (x > 0 && x < ancho-1 && y > 0 && y < alto-1 ) {
+                    for (int i = -1 ; i < 2; i++) {
+                        if (this.getActual(x-1,y+i)=='A') {
+                            return 'A';                           
+                        }else if(this.getActual(x-1,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                              return 'A';
+                            }
+                        }
+                        if (this.getActual(x,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                        if (this.getActual(x+1,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+1,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                    }
+                }else if(x == 0 && y > 0 && y < alto-1){
+                    for (int i = -1 ; i < 2; i++) {
+                        if (this.getActual(x,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                        if (this.getActual(x+1,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+1,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A'; 
+                            }
+                        }
+                    }
+                }else if(y == 0 && x > 0 && x < ancho-1){
+                    for (int i = -1 ; i < 2; i++) {
+                        if (this.getActual(x+i,y)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+i,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                        if (this.getActual(x+i,y+1)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+i,y+1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                    }
+                }else if (y == alto-1 && x>0  && x < ancho-1 ) {
+                    for (int i = -1 ; i < 2; i++) {
+                        if (this.getActual(x+i,y-1)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+i,y-1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                        if (this.getActual(x+i,y)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x+i,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                    }
+                }else if (x == ancho-1 && y>0 && y< alto-1) {
+                    for (int i = -1 ; i < 2; i++) {
+                        if (this.getActual(x,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                        }
+                        if (this.getActual(x-1,y+i)=='A') {
+                            return 'A';
+                        }else if(this.getActual(x-1,y+i)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A'; 
+                            }
+                        }
+                    }
+                }else if (x == 0 && y == 0) {
+                    if (this.getActual(x+1,y)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x+1,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x+1,y+1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x+1,y+1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x,y+1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x,y+1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                }else if (x == ancho-1 && y == alto-1) {
+                    if (this.getActual(x-1,y)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x-1,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x-1,y-1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x-1,y-1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x,y-1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x,y-1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                }else if (x == 0 && y == alto-1) {
+                    if (this.getActual(x+1,y)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x+1,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x+1,y-1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x+1,y-1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x,y-1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x,y-1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                }else if (x == ancho-1 && y == 0) {
+                    if (this.getActual(x-1,y)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x-1,y)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x-1,y+1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x-1,y+1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                    if (this.getActual(x,y+1)=='A') {
+                      return 'A';
+                    }else if(this.getActual(x,y+1)=='B'){
+                            aux++;
+                            if (aux == rb) {
+                               return 'A';
+                            }
+                    }
+                }
+                
+                break;
+            case 'A':
+                this.setLagActual(x, y, true);                
+                if (this.getLagActual(x, y)== 4) {
+                    this.setSiguiente(x, y, 'B');
+                    this.setLagActual(x, y, false);
+                    return 'B';
+                }
+                return 'A';   
+                
+            case 'B':
+                this.setSiguiente(x, y, 'D');
+                return 'D';
+            case 'D':
+                Random nuevaCelula = new Random();
+                if (nuevaCelula.nextInt(prepl) == 1) {
+                    Random nuevaA = new Random();
+                    if (nuevaA.nextInt(pinfect)== 1) {
+                        this.setSiguiente(x, y, 'A');
+                        return 'A';
+                    }else{
+                        this.setActual(x, y, 'H');
+                        return'H';
+                    }
+                }else{
+                    return 'D';
+                } 
+        }
+        return this.getActual(x, y); 
+    }
+    public void actualizarEstado(int y, int x, char c){
+        for (int i = 0; i < alto; i++) {
+            for (int j = 0; j < ancho; j++) {
+                this.setActual(x, y, c);
             }
         }
     }
     
-   
+    public void insertarCelulasA(){
+        float porcentaje = (pvih*alto*ancho)/100;
+        Random x = new Random();
+        Random y = new Random();
+        for (int i = 0; i < Math.round(porcentaje); i++) {
+            this.setActual( x.nextInt(ancho),y.nextInt(alto) ,'A');
+        }
+    }
 }
--- src/simulacionvih/vistas/Simulacion.java
+++ src/simulacionvih/vistas/Simulacion.java
@@ -3,25 +3,55 @@ package simulacionvih.vistas;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Graphics;
+import java.util.Timer;
+import java.util.TimerTask;
 import simulacionvih.VIH;
 
 public class Simulacion extends javax.swing.JFrame {
     private final int ancho = 200;
-    private final int alto = 100;
-    private VIH cel = new VIH(ancho, alto);
+    private final int alto = 200;
+    private VIH cel;
     private Image tableroI;
     private Graphics tableroG;
+    private boolean inicio;
+    private Timer t; 
+    private TimerTask task;
     
     public Simulacion() {
         initComponents();
+        cel = new VIH(ancho, alto);
+        inicio = true;
         tableroI = createImage(jPanel1.getWidth(),jPanel1.getHeight() );
         tableroG = tableroI.getGraphics();
+        t = new Timer();
+        task = new TimerTask(){
+            @Override
+            public void run(){
+                
+                if (btnInicio.getText().equals("Pausa")) {
+                    for (int i = 0; i < alto; i++) {
+                        for (int j = 0; j < ancho; j++) {
+                           cel.setSiguiente(j, i, cel.definirEstadoSig(i, j));
+                        }
+                    }
+                    for (int i = 0; i < alto; i++) {
+                        for (int j = 0; j < ancho; j++) {
+                           cel.actualizarEstado(i, j,cel.getSiguiente(j, i));
+                        }
+                    }
+                    dibujarTablero();
+                }
+            }
+        };
+        t.scheduleAtFixedRate(task, 0, 400);
         dibujarTablero();
     }
     
     private void dibujarTablero(){
         tableroG.setColor(jPanel1.getBackground());
-        tableroG.fillRect(0, 0, jPanel1.getWidth(), jPanel1.getHeight());     
+        tableroG.fillRect(0, 0, jPanel1.getWidth(), jPanel1.getHeight());  
+        
+        //Dibuja Celulas
         for (int i = 0; i < alto; i++) {
             for (int j = 0; j < ancho; j++) {
                 if (cel.getActual(i,j)== 'A') {
@@ -29,9 +59,25 @@ public class Simulacion extends javax.swing.JFrame {
                     int y = i* jPanel1.getHeight()/alto;
                     int x = j* jPanel1.getWidth()/ancho;
                     tableroG.fillRect(x, y, jPanel1.getWidth()/ancho, jPanel1.getHeight()/alto);
+                }else if (cel.getActual(i,j)== 'B') {
+                    tableroG.setColor(Color.GREEN);
+                    int y = i* jPanel1.getHeight()/alto;
+                    int x = j* jPanel1.getWidth()/ancho;
+                    tableroG.fillRect(x, y, jPanel1.getWidth()/ancho, jPanel1.getHeight()/alto);
+                }else if (cel.getActual(i,j)== 'D') {
+                    tableroG.setColor(Color.RED);
+                    int y = i* jPanel1.getHeight()/alto;
+                    int x = j* jPanel1.getWidth()/ancho;
+                    tableroG.fillRect(x, y, jPanel1.getWidth()/ancho, jPanel1.getHeight()/alto);
+                }else{
+                    tableroG.setColor(jPanel1.getBackground());
+                    int y = i* jPanel1.getHeight()/alto;
+                    int x = j* jPanel1.getWidth()/ancho;
+                    tableroG.fillRect(x, y, jPanel1.getWidth()/ancho, jPanel1.getHeight()/alto);
                 }
             }
         }
+        
         tableroG.setColor(Color.black);
         //dibuja barras Horizontales 
         for (int i = 0; i < alto; i++) {
@@ -54,8 +100,8 @@ public class Simulacion extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         btnInicio = new javax.swing.JButton();
-        btnDetener = new javax.swing.JButton();
         btnClean = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -90,9 +136,19 @@ public class Simulacion extends javax.swing.JFrame {
             }
         });
 
-        btnDetener.setText("Detener");
-
         btnClean.setText("Limpiar");
+        btnClean.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnCleanActionPerformed(evt);
+            }
+        });
+
+        jButton1.setText("Agregar Celulas A ");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -103,10 +159,10 @@ public class Simulacion extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 178, Short.MAX_VALUE)
-                        .addComponent(btnClean, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 142, Short.MAX_VALUE)
+                        .addComponent(jButton1)
                         .addGap(18, 18, 18)
-                        .addComponent(btnDetener, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btnClean, javax.swing.GroupLayout.PREFERRED_SIZE, 82, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
@@ -117,9 +173,9 @@ public class Simulacion extends javax.swing.JFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(btnInicio, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
-                    .addComponent(btnClean, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btnDetener, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(btnInicio, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
+                    .addComponent(btnClean, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
 
@@ -127,7 +183,13 @@ public class Simulacion extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnInicioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnInicioActionPerformed
-        // TODO add your handling code here:
+        inicio = !inicio;
+        if (inicio) {
+            btnInicio.setText("Inicio");
+        }else{
+            btnInicio.setText("Pausa");
+        }
+        dibujarTablero();
     }//GEN-LAST:event_btnInicioActionPerformed
 
     private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseClicked
@@ -140,14 +202,23 @@ public class Simulacion extends javax.swing.JFrame {
     private void jPanel1ComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel1ComponentResized
         tableroI = createImage(jPanel1.getWidth(),jPanel1.getHeight() );
         tableroG = tableroI.getGraphics();
-        
         dibujarTablero();
     }//GEN-LAST:event_jPanel1ComponentResized
 
+    private void btnCleanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCleanActionPerformed
+        cel.llenar();
+        dibujarTablero();
+    }//GEN-LAST:event_btnCleanActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        cel.insertarCelulasA();
+        dibujarTablero();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnClean;
-    private javax.swing.JButton btnDetener;
     private javax.swing.JButton btnInicio;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
